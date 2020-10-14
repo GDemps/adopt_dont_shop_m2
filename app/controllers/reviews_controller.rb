2b1ad00f569544921bf213f08711cc7e09c679e6
James Belta <james.belta@gmail.com>
2020-10-13 18:32:56 -0600

    "forced" user.id into :user_id. Assuming only one user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,18 +2,20 @@ class ReviewsController < ApplicationController
 
   def new
     @shelter = Shelter.find(params[:shelter_id])
-    binding.pry
   end
 
   def create
     @shelter = Shelter.find(params[:shelter_id])
-    binding.pry
-    @review = Review.new(review_params)
+    @user = User.first
+    review = @shelter.reviews.new(review_params)
+    review[:user_id] = @user.id
+    review.save
+    redirect_to "/shelters/#{@shelter.id}"
   end
 
   private
   def review_params
-    params.permit(:title, :rating, :content, :image, :name,)
+    params.permit(:title, :rating, :content, :image, :name)
   end
 
 
