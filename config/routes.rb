ea5a7e9345d0aa4f02409618a90ed0e997218579
Grant Dempsey <CryptoDemps@gmail.com>
2020-10-13 21:26:50 -0600

    Took /reviews off the end of the update route

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,5 @@ Rails.application.routes.draw do
   get '/shelters/:shelter_id/reviews/new', to: 'reviews#new'
   post '/shelters/:shelter_id/reviews', to: 'reviews#create'
   get '/reviews/:id/edit', to: "reviews#edit"
-  patch '/reviews/:id/reviews', to: "reviews#update"
+  patch '/reviews/:id', to: "reviews#update"
 end
