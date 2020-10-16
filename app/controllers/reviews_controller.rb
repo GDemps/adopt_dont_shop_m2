e827798bf743959deb7134b67824e7e3df6f00fe
Grant Dempsey <CryptoDemps@gmail.com>
2020-10-15 19:01:17 -0600

    Reworked if statements to exclude user not in db

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,19 +10,18 @@ class ReviewsController < ApplicationController
 
   def create
     @shelter = Shelter.find(params[:shelter_id])
-    @user = User.where(name: params[:name]).first
-    @review = @shelter.reviews.new(review_params)
-    @review[:user_id] = @user.id
-    if @review.name_exists? && @review.name_match? && @review.save
-      redirect_to "/shelters/#{@shelter.id}"
-    elsif @review.name_exists? == false
-      flash.now[:notice] = "No user with the name #{@review.name}"
-      render :new
-    elsif @review.name_match? == false
-      flash.now[:notice] = "Only #{@review.name} can sign this review"
-      render :new
-    elsif @review.save == false
-      flash.now[:notice] = @review.errors.full_messages.uniq.to_sentence
+    if User.where(name: params[:name]) != []
+      @user = User.where(name: params[:name]).first
+      @review = @shelter.reviews.new(review_params)
+      @review[:user_id] = @user.id
+      if @review.save
+        redirect_to "/shelters/#{@shelter.id}"
+      else
+        flash.now[:notice] = @review.errors.full_messages.uniq.to_sentence
+        render :new
+      end
+    else
+      flash.now[:notice] = "No user with the name #{params[:name]}"
       render :new
     end
   end
@@ -32,11 +31,17 @@ class ReviewsController < ApplicationController
   end
 
   def update
-    @review = Review.find(params[:id])
-    if @review.update(review_params)
-      redirect_to "/shelters/#{@review.shelter_id}"
+    if User.where(name: params[:name]) != []
+      @review = Review.find(params[:id])
+      if @review.update(review_params)
+        redirect_to "/shelters/#{@review.shelter_id}"
+      else
+        flash.now[:notice] = @review.errors.full_messages.uniq.to_sentence
+        render :edit
+      end
     else
-      flash.now[:notice] = @review.errors.full_messages.uniq.to_sentence
+      @review = Review.find(params[:id])
+      flash.now[:notice] = "No user with the name #{params[:name]}"
       render :edit
     end
   end
