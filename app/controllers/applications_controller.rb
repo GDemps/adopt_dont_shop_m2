8ea383730ded420fea391115548ffd57eda676cc
Grant Dempsey <CryptoDemps@gmail.com>
2020-10-19 19:53:12 -0600

    Added new and create methods

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,23 @@
 class ApplicationsController < ApplicationController
   def show
-    binding.pry
     @application = Application.find(params[:id])
+  end
+
+  def new
+    @pet = Pet.find(params[:id])
+  end
+
+  def create
+    @user = User.find_by(name: params[:applicant])
+    @application = @user.applications.new(application_params)
+    @application.application_status = "In Progress"
+    @application.save
+    ApplicationPet.create(application_id: @application.id, pet_id: params[:id])
+    redirect_to "/applications/#{@application.id}"
+  end
+
+  private
+  def application_params
+    params.permit(:applicant, :address, :description)
   end
 end
