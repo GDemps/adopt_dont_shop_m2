791b11dbb69d636a503baaa170754cb6c4c1404e
Grant Dempsey <CryptoDemps@gmail.com>
2020-10-20 18:45:32 -0600

    Wrote 3 model methods for shelters

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,4 +1,16 @@
 class Shelter < ApplicationRecord
   has_many :pets
   has_many :reviews
+
+  def num_of_pets
+    pets.count
+  end
+
+  def avg_rating
+    reviews.average(:rating).to_f
+  end
+
+  def total_apps
+    pets.select('pets.*, applications.*').joins(:applications).to_a.count
+  end
 end
